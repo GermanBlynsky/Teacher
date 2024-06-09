a4d4709211de81778e0d5c484b830282bffedeb9
Герман Блынский <german.b05@mail.ru>
2024-06-10 02:58:42 +0300

    поменял main

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -19,5 +19,20 @@ int main()
 	Teacher teacher = Teacher(name, birth, post, degree, rank, gender);
 	std::string c = teacher.toString();
 	std::cout << c;
+	std::string name2 = "Маша Непупкина";
+	Date birth2 = Date(23, 12, 1952);
+	Post post2 = Post(ProfessorPost);
+	Degree degree2 = Degree(ProfessorDegree);
+	Rank rank2 = Rank(DoctorRank);
+	Gender gender2 = Gender(Female);
+	Teacher teacher2 = Teacher(name2, birth2, post2, degree2, rank2, gender2);
+	std::string a = teacher.toString();
+	std::cout << a << "\n";
+	if (teacher.equalByDate(teacher2)) {
+		std::cout << " Даты рождения совпадают";
+	}
+	else {
+		std::cout << "Даты рождения несовпадают";
+	}
 	return 0;
 }
