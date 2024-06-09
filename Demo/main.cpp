930ca01b724215061a183690f2da55a725fc9a73
Герман Блынский <german.b05@mail.ru>
2024-06-10 02:41:59 +0300

    подправил язык

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -3,14 +3,14 @@
 #include "Teacher.h"
 
 /**
-* @brief –¢–Њ—З–Ї–∞ –≤—Е–Њ–і–∞ –≤ –њ—А–Њ–≥—А–∞–Љ–Љ—Г
-* @return 0 –≤ —Б–ї—Г—З–∞–µ —Г—Б–њ–µ—Е–∞
+* @brief Точка входа в программу
+* @return 0 при успешном выполнении
 */
 int main()
 {
 	setlocale(LC_ALL, "ru_RU.UTF-8");
 	using namespace uizi;
-	std::string name = "–Т–∞—Б—П –Я—Г–њ–Ї–Є–љ";
+	std::string name = "Вася Пупкин";
 	Date birth = Date(23, 12, 1952);
 	Post post = Post(DocentPost);
 	Degree degree = Degree(DocentDegree);
