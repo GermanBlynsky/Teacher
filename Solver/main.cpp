13dd274ef54bbc1a76d867f4a17cae4cf3962020
Герман Блынский <german.b05@mail.ru>
2024-06-09 18:51:40 +0300

    пытаюсь починить тесты

--- Solver/main.cpp
+++ Solver/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
-#include "Teacher.h"
+#include "..\Demo\Teacher.h"
 
 /**
-* @brief РўРѕС‡РєР° РІС…РѕРґР° РІ РїСЂРѕРіСЂР°РјРјСѓ
-* @return 0 РІ СЃР»СѓС‡Р°Рµ СѓСЃРїРµС…Р°
+* @brief Точка входа в программу
+* @return 0 в случае успеха
 */
 int main()
 {
 	setlocale(LC_ALL, "ru_RU.UTF-8");
 	using namespace uizi;
-	std::string name = "Р’Р°СЃСЏ РџСѓРїРєРёРЅ";
+	std::string name = "Вася Пупкин";
 	Date birth = Date(23, 12, 1952);
 	Post post = Post(DocentPost);
 	Degree degree = Degree(DocentDegree);
 	Rank rank = Rank(CandidatRank);
-	Gender gender = Gender(Male);
+	Gender gender = Gender(NonBinary);
 	Teacher teacher = Teacher(name, birth, post, degree, rank, gender);
 	std::string c = teacher.toString();
 	std::cout << c;
+	std::cout << "11111";
 	return 0;
 }
